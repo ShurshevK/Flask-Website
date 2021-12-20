1ede9b8295e956d792c325b04115041c11aa8bf8
ShurshevK <shurshevkirill@gmail.com>
2021-12-20 20:36:14 +0100

    init

--- API-JSON-Server/app.py
+++ API-JSON-Server/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from mock_data import lists, list_items, abort
+from flask import Flask, json, abort
+from mock_data import lists, list_items
 
 
 app = Flask(__name__)
@@ -23,6 +23,18 @@ def get_list(list_id):
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': '404 Not Found'}), 404
+
+def get_list(list_id):
+    _list = {}
+    search = [_list for _list in lists if _list['id'] == list_id]
+    if len(search) > 0:
+        _list = search[0]
+    else:
+        abort(404)
+    _items = [_item for _item in list_items if _item['list_id'] == list_id]
+    if len(_items) > 0:
+        _list['list_items'] = _items
+    return jsonify({'list': _list})
 
 if __name__ == '__main__':
     app.run(debug=True)
